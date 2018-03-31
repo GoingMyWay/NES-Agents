a59c33c87a91eb990cd3c1ce4915165fb3bcf6ce
GoingMyWay <scutqiuwei@163.com>
2018-03-31 10:31:07 +0800

    nes agents

--- agents.py
+++ agents.py
@@ -1,4 +1,6 @@
 # THIS IS SINGLE CPU VERSION
+import random
+
 import gym
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
@@ -7,19 +9,20 @@ from nes import NES
 
 
 class PongAgent(object):
-    def __init__(self, img_shape=(40, 40), hist_len=4):
+    def __init__(self, img_shape=(84, 84), hist_len=4):
         self.env = gym.make('PongDeterministic-v0')
-        self.es = NES(weights=self.weights,
-                      reward_func=self.reward_func,
-                      alpha=0.01,
-                      sigma=0.01,
-                      population_size=15)
         self._weights = []
         self._img_shape = img_shape
         self._hist_len = hist_len
         self._create_network()
         self.sess = tf.Session()
         self.sess.run(tf.global_variables_initializer())
+        self.es = NES(sess=self.sess,
+                      weights=self.weights,
+                      reward_func=self.reward_func,
+                      alpha=0.01,
+                      sigma=0.01,
+                      population_size=15)
 
     def play(self):
         pass
@@ -31,20 +34,18 @@ class PongAgent(object):
     def predict_action(self, input_state):
         pass
 
-    def reward_func(self):
-        pass
+    def reward_func(self, weights):
+        return random.random()
 
     def _create_network(self, scope='network'):
         with tf.variable_scope(scope):
             inputs = tf.placeholder(shape=[None, *self.img_shape, self.hist_len], dtype=tf.float32)
-            conv_1 = slim.conv2d(activation_fn=tf.nn.relu, inputs=inputs, num_outputs=32,
+            conv_1 = slim.conv2d(activation_fn=tf.nn.relu, inputs=inputs, num_outputs=16,
                                  kernel_size=[8, 8], stride=4, padding='SAME')
             conv_2 = slim.conv2d(activation_fn=tf.nn.relu, inputs=conv_1, num_outputs=64,
                                  kernel_size=[4, 4], stride=2, padding='SAME')
-            conv_3 = slim.conv2d(activation_fn=tf.nn.relu, inputs=conv_2, num_outputs=64,
-                                 kernel_size=[3, 3], stride=1, padding='SAME')
-            fc = slim.fully_connected(slim.flatten(conv_3), 64, activation_fn=tf.nn.elu)
-
+            fc = slim.fully_connected(slim.flatten(conv_2), 256, activation_fn=tf.nn.elu)
+            self.weights = tf.trainable_variables()
 
     @property
     def weights(self):
@@ -69,3 +70,7 @@ class PongAgent(object):
     @hist_len.setter
     def hist_len(self, hist_len):
         self._hist_len = hist_len
+
+
+def gym_process():
+    pass
