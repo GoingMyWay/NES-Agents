6af15edce79b6568587d1cd8ae001cfc4d15edf5
GoingMyWay <scutqiuwei@163.com>
2018-03-31 10:31:04 +0800

    nes

--- nes/nes.py
+++ nes/nes.py
@@ -3,34 +3,39 @@ import numpy as np
 
 
 class NES(object):
-    def __init__(self, weights, reward_func, alpha, sigma, population_size):
+    def __init__(self, sess, weights, reward_func, alpha, sigma, population_size):
+        self._sess = sess
         self._weights = weights
+        self._real_weights = self._sess.run(self._weights)
         self._reward_func = reward_func
         self._alpha = alpha
         self._sigma = sigma
         self._population_size = population_size
 
     def w_try(self, i_popu):
-        return [self.weights[i] + self.sigma * noise for i, noise in enumerate(i_popu)]
+        return [self._real_weights[i] + self.sigma * noise for i, noise in enumerate(i_popu)]
 
     def train(self, n_iters=300, p_steps=20):
         for iter in range(n_iters):
             if iter % p_steps == 0:
-                print(' iter: %s, reward: %s' % (iter, self.reward_func(self.weights)))
+                print(' iter: %s, reward: %s' % (iter, self._reward_func(self._real_weights)))
 
             # randomly initialize weights of each population
-            populations = [[np.random.randn(*w.shape) for w in self.weights] for _ in range(self.population_size)]
-            # initialize rewards
-            rewards = np.zeros(self.population_size)
-            # jitter weights
-            for i, i_popu in enumerate(populations):
-                w_try = self.w_try(i_popu)
-                rewards[i] = self.reward_func(w_try)
+            populations = [[np.random.randn(*w.shape) for w in self._real_weights] for _ in range(self.population_size)]
 
+            # jitter weights and get the rewards
+            rewards = [
+                self._reward_func(self.w_try(i_popu)) for i, i_popu in enumerate(populations)
+            ]
             A = (rewards - np.mean(rewards)) / np.std(rewards)
-            for i, _ in enumerate(self.weights):
+
+            # update the value of weights
+            for i, _ in enumerate(self._real_weights):
                 N = np.array([pop[i] for pop in populations])
-                self.weights[i] += self.alpha/(self.population_size*self.sigma) * np.dot(N.T, A).T
+                self._real_weights[i] += self.alpha/(self.population_size*self.sigma) * np.dot(N.T, A).T
+
+            # assign value to tensorflow's graph
+            self._sess.run([tf_var.assign(real_var) for tf_var, real_var in zip(self.weights, self._real_weights)])
 
     @property
     def weights(self):
@@ -39,14 +44,6 @@ class NES(object):
     @weights.setter
     def weights(self, weights):
         self._weights = weights
-
-    @property
-    def reward_func(self):
-        return self._reward_func
-
-    @reward_func.setter
-    def reward_func(self, reward_func):
-        self._reward_func = reward_func
 
     @property
     def alpha(self):
